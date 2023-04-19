c5c9f0ac1ca82bd2608000f60c224fe3c40e21ed
mohitgupta2206 <guptamohit2206@gmail.com>
2023-04-19 20:20:15 +0530

    chapter-2

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-//  <script>
-
-//   var element=document.createElement('h2');
-//   element.innerText="India is such a big developing Country";
-//   document.getElementById('root').appendChild(element);
-//    </script> 
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
     const heading1=React.createElement('h1',{
         key:1,
         className:"heading1",
-     
+    
        },'heading1');
 
        const heading2=React.createElement('h2',{
         key:2,
         className:"heading2",
+        id:"add",
      
        },'heading2');
 
@@ -22,5 +18,8 @@
         className:"container",
      
        },[heading1,heading2]);
+      
+    
     const root=ReactDOM.createRoot(document.getElementById('root'));
+  
     root.render(container);
